acd9cff20912266d9f8afeb4a8ea63e3fa7a315a
Saharsh Laud <56641680+SaharshLaud@users.noreply.github.com>
2022-05-29 02:25:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from statistics import mode
 from flask import Flask, render_template, request
+import cv2
 from keras.models import load_model
 import numpy as np
 
@@ -21,8 +22,39 @@ def after():
     img.save('static/file.jpg')
 
 
-   
+    # using opencv to detect faces in image
+     
+    img1 = cv2.imread('static/file.jpg')
+    gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+    faces = cascade.detectMultiScale(gray, 1.1, 3)
+
+    for x,y,w,h in faces:
+        cv2.rectangle(img1, (x,y), (x+w, y+h), (0,255,0), 2)
+
+        cropped = img1[y:y+h, x:x+w]
+
+    cv2.imwrite('static/after.jpg', img1)
+
+    try:
+        cv2.imwrite('static/cropped.jpg', cropped)
+
+    except:
+        pass
+
+
+    # load the image using cv2 library and reshape to 48*48 pixel size
+    try:
+        img = cv2.imread('static/cropped.jpg', 0)
+    except:
+        img = cv2.imread('static/file.jpg', 0)
+
+
+
+
+
     
+    img = cv2.resize(img,(48,48))
     img = img/255.0
 
     img = np.reshape(img, (1,48,48,1))
